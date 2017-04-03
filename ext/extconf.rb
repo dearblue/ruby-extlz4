4a5c8fc81665937c4ae34b59d6b353f84361c33e
dearblue <dearblue@users.noreply.github.com>
2017-04-03 20:09:07 +0900

    パスの修正

--- ext/extconf.rb
+++ ext/extconf.rb
@@ -5,10 +5,9 @@ require "mkmf"
 
 # TODO: システムにインストールされた lz4 がある場合、バージョンを確認してより新しければそちらを利用する
 
-$srcs = Dir.glob(File.join(File.dirname(__FILE__).gsub(/[\[\{\?\*]/, "[\\0]"), "{.,../contrib/*}/*.c")).map { |n| File.basename n }
-$VPATH << "$(srcdir)/../contrib/lz4"
-find_header "lz4.h", "$(srcdir)/../contrib/lz4" or abort 1
-find_header "xxhash.h", "$(srcdir)/../contrib/lz4" or abort 1
+$srcs = Dir.glob(File.join(File.dirname(__FILE__).gsub(/[\[\{\?\*]/, "[\\0]"), "{.,../contrib/lz4/lib}/*.c")).map { |n| File.basename n }
+$VPATH << "$(srcdir)/../contrib/lz4/lib"
+$CPPFLAGS << " -I$(srcdir)/../contrib/lz4/lib"
 
 if RbConfig::CONFIG["arch"] =~ /mingw/
   $LDFLAGS << " -static-libgcc"
