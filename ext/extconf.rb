70e2c2e1497a1c3b4305fe84778f6308c7ef7463
dearblue <dearblue@users.noreply.github.com>
2017-04-15 22:25:25 +0900

    lz4 ライブラリのソースコードを融合させてコンパイルさせるように修正

--- ext/extconf.rb
+++ ext/extconf.rb
@@ -5,9 +5,7 @@ require "mkmf"
 
 # TODO: システムにインストールされた lz4 がある場合、バージョンを確認してより新しければそちらを利用する
 
-$srcs = Dir.glob(File.join(File.dirname(__FILE__).gsub(/[\[\{\?\*]/, "[\\0]"), "{.,../contrib/lz4/lib}/*.c")).map { |n| File.basename n }
-$VPATH << "$(srcdir)/../contrib/lz4/lib"
-$CPPFLAGS << " -I$(srcdir)/../contrib/lz4/lib"
+append_cppflags "-I$(srcdir)/../contrib/lz4/lib"
 
 if RbConfig::CONFIG["arch"] =~ /mingw/
   $LDFLAGS << " -static-libgcc"
---
+++ ext/lz4_amalgam.c
@@ -0,0 +1,26 @@
+#include "../contrib/lz4/lib/lz4.c"
+
+#define LZ4_isLittleEndian      amalg_LZ4_isLittleEndian
+#define LZ4_read16              amalg_LZ4_read16
+#define LZ4_read32              amalg_LZ4_read32
+#define LZ4_read_ARCH           amalg_LZ4_read_ARCH
+#define LZ4_write16             amalg_LZ4_write16
+#define LZ4_write32             amalg_LZ4_write32
+#define LZ4_readLE16            amalg_LZ4_readLE16
+#define LZ4_writeLE16           amalg_LZ4_writeLE16
+#define LZ4_copy8               amalg_LZ4_copy8
+#define LZ4_wildCopy            amalg_LZ4_wildCopy
+#define LZ4_minLength           amalg_LZ4_minLength
+#define LZ4_NbCommonBytes       amalg_LZ4_NbCommonBytes
+#define LZ4_count               amalg_LZ4_count
+#define limitedOutput           amalg_limitedOutput
+#define limitedOutput_directive amalg_limitedOutput_directive
+#include "../contrib/lz4/lib/lz4hc.c"
+
+#undef ALLOCATOR
+#undef KB
+#undef MB
+#undef GB
+#include "../contrib/lz4/lib/lz4frame.c"
+
+#include "../contrib/lz4/lib/xxhash.c"
